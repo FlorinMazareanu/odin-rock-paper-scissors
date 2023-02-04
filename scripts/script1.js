fa46fa3fc4d8525c741ef755e770151e36fa7902
FlorinMazareanu <florin.mazareanu@gmail.com>
2023-02-04 16:27:21 +0200

    3.1

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,8 +1,26 @@
 console.log("test");
+
+function getPlayerChoice(pc) {
+    console.log("se executa getPlayerChoice");
+    if (pc == 0) {
+        console.log("ai ales piatra");
+        document.getElementById("status-joc").innerHTML = "rock";
+    }
+    else if (pc == 1) {
+        console.log("ai ales hartie");
+        document.getElementById("status-joc").innerHTML = "paper";
+    }
+    else {
+        console.log("ai ales foarfeca");
+        document.getElementById("status-joc").innerHTML = "scissor";
+    }
+}
+
 function getComputerChoice() {
     console.log("se executa getComputerChoice");
     //console.log(Math.floor(Math.random() * 3));
     let computerChoice = Math.floor(Math.random() * 3);
     console.log(computerChoice);
 }
+
 getComputerChoice();
