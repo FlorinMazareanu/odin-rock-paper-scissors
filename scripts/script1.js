3a4214cf1c3cab7540b72811ccf83a14cfc1d3cd
FlorinMazareanu <florin.mazareanu@gmail.com>
2023-02-04 17:25:01 +0200

    4.0

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,26 +1,65 @@
-console.log("test");
-
-function getPlayerChoice(pc) {
-    console.log("se executa getPlayerChoice");
-    if (pc == 0) {
-        console.log("ai ales piatra");
-        document.getElementById("status-joc").innerHTML = "rock";
-    }
-    else if (pc == 1) {
-        console.log("ai ales hartie");
-        document.getElementById("status-joc").innerHTML = "paper";
-    }
-    else {
-        console.log("ai ales foarfeca");
-        document.getElementById("status-joc").innerHTML = "scissor";
-    }
-}
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    console.log("se executa getComputerChoice");
-    //console.log(Math.floor(Math.random() * 3));
     let computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
+    return computerChoice;
 }
 
-getComputerChoice();
+function getPlayerChoice(playerChoice) {
+    let computerChoice = getComputerChoice();
+    if (playerChoice == 0) {
+        switch(computerChoice) {
+            case 0:
+                document.getElementById("status-joc").innerHTML = "You both chose rock";
+                break;
+            case 1:
+                document.getElementById("status-joc").innerHTML = "You chose rock, computer chose paper";
+                computerScore++;
+                break;
+            case 2:
+                document.getElementById("status-joc").innerHTML = "You chose rock, computer chose scissors";
+                playerScore++;
+                break;
+            default:
+                console.log("???");
+        }
+    }
+    else if (playerChoice == 1) {
+        switch(computerChoice) {
+            case 0:
+                document.getElementById("status-joc").innerHTML = "You chose paper, computer chose rock";
+                playerScore++;
+                break;
+            case 1:
+                document.getElementById("status-joc").innerHTML = "You both chose paper";
+                break;
+            case 2:
+                document.getElementById("status-joc").innerHTML = "You chose paper, computer chose scissors";
+                computerScore++;
+                break;
+            default:
+                console.log("???");
+        }
+    }
+    else {
+        switch(computerChoice) {
+            case 0:
+                document.getElementById("status-joc").innerHTML = "You chose scissors, computer chose rock";
+                computerScore++;
+                break;
+            case 1:
+                document.getElementById("status-joc").innerHTML = "You chose scissors, computer chose paper";
+                playerScore++;
+                break;
+            case 2:
+                document.getElementById("status-joc").innerHTML = "You both schose scissors";
+                break;
+            default:
+                console.log("???");
+        }
+    }
+    document.getElementById("scor-player").innerHTML = playerScore;
+    document.getElementById("scor-computer").innerHTML = computerScore;
+}
+
