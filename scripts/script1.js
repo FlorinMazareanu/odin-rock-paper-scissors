c428acfa87e14f3c0d2b3d48d54239b01d4dd5fd
FlorinMazareanu <florin.mazareanu@gmail.com>
2023-02-04 14:39:34 +0200

    facut structura foldere

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,1 +1,8 @@
 console.log("test");
+function getComputerChoice() {
+    console.log("se executa getComputerChoice");
+    //console.log(Math.floor(Math.random() * 3));
+    let computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+}
+getComputerChoice();
